3e16a3ac662b6b5ce6e1962d9b8490a8e166fa1d
frankiesardo <fran.sardo@gmail.com>
2014-11-30 15:24:57 +0000

    It's icepick processor, stupid.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject icepick "0.1.0-SNAPSHOT"
+(defproject com.github.frankiesardo/icepick-processor "2.4.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[com.google.testing.compile/compile-testing "0.4"]
-                                  [com.github.frankiesardo/icepick "2.3.6"]
-                                  [com.google.android/android "4.1.1.4"]]}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.github.frankiesardo/icepick "2.3.6"]]
+  :profiles {:dev {:dependencies [[com.google.testing.compile/compile-testing "0.4"]]}
+             :provided {:dependencies [[com.google.android/android "4.1.1.4"]]}
              :uberjar {:aot :all}})
